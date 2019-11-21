831414fb5773cb7595c0d57ba9f58d44fbed4f28
Edicarla Sillva <edicarlasillva@gmail.com>
2019-11-21 13:48:56 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,21 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
-    <Search/>
+    <h1>Teste</h1>
+    <router-view />
+    <search />
   </div>
 </template>
 
 <script>
-import Search from './components/Search'
+import Search from "./components/Search";
+import Card from "./components/Card";
 export default {
- name: 'App',
- components: {
-  Search
- }
-}
+  name: "App",
+  components: {
+    Search,
+    Card
+  }
+};
 </script>
 
-<script>
-import Search from './components/Search'
-export default {
- name: 'App',
- components: {
-  Search
- }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,31 +1,62 @@
 <template>
- <div class='search'>
-  <h1>Search</h1>
-  <input type='text' v-model='query' @keyup='getResult(query)'>
-  <div v-for='result in results' :key='result.id'>
-   <p>{{result.title}}</p>
-   <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+  <div class="search">
+    <h1>Search</h1>
+    <input type="text" v-model="query" @keyup="getResult" />
+    <section class="card" v-for="result in results" :key="result.id">
+      <figure class="card__image">
+        <img
+          v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path"
+          width="100px"
+        />
+      </figure>
+      <div class="card__container">
+        <header class="card__header">
+          <div class="card__title">
+            <h1>{{ result.title }}</h1>
+          </div>
+          <div class="card__vote-average">{{ result.vote_average }}</div>
+          <div class="card__date">{{ result.release_date }}</div>
+        </header>
+        <div class="card__main">
+          <div class="card__overview">
+            {{ result.overview }}
+          </div>
+          <div class="card__genres">
+            <ul>
+              <li v-for="genre in genres" :key="genre.id">{{ genre.name }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
- </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
- name: 'search',
- data () {
- return {
-  query: '',
-  results: ''
-}
- },
-methods: {
-  getResult(query) {
-   axios.get('https://api.themoviedb.org/3/search/movie?  api_key=08555db9f6be8fa06d4c47bc7e2d3335q=' + query)
-        .then(response => { this.results = response.data.results })
-     console.log(this.results)
-   }
- }
-}
+  name: "search",
+  data() {
+    return {
+      query: "",
+      results: ""
+    };
+  },
+  methods: {
+    getResult() {
+      if (this.query.length >= 3) {
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/movie?api_key=08555db9f6be8fa06d4c47bc7e2d3335&query=${this.query}`
+          )
+          .then(response => {
+            this.results = response.data.results;
+          })
+          .catch(error => alert("Falha ao consultar os dados na api."));
+      } else if (this.query.length == 0) {
+        this.results = "";
+      }
+    }
+  }
+};
 </script>
-
