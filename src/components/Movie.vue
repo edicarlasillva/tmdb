8c668178944c53e6249689cb887ff5ed066687fe
Edicarla Sillva <edicarlasillva@gmail.com>
2019-11-22 17:09:53 -0300

    Update trailer

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -62,7 +62,7 @@
       </article>
     </div>
     <div class="inner-page__bottom">
-     <iframe width="560" height="315" src="`https://www.youtube.com/embed/${key}`" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <iframe :src="`https://www.youtube.com/embed/${key}`" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
